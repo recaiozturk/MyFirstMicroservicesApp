8f23b3957329b7fdec3ec7b18408de7328d216d9
Recai <recaiozturk54@gmail.com>
2024-04-17 13:56:20 +0300

    Add project files.

---
+++ Services/Catalog/MyMicroService.Services.Catalog/Models/Feature.cs
@@ -0,0 +1,7 @@
+﻿namespace MyMicroService.Services.Catalog.Models
+{
+    public class Feature
+    {
+        public string Duration { get; set; }
+    }
+}
---
+++ Shared/MyMicroservice.Shared/Dtos/ResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.Json.Serialization;
+
+namespace MyMicroservice.Shared.Dtos
+{
+    public class ResponseDto<T>
+    {
+        public T Data { get; set; }
+
+        //json ignore ile cliente status code gösttermeye gerek yok,zaten code olusuyor,sadece biz kullancaz
+        [JsonIgnore]
+        public int StatusCode { get; set; }
+
+        [JsonIgnore]
+        public bool IsSuccesful { get; set; }
+    }
+}
